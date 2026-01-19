d5c152ce6cd768d91ef84320d25e98d0a6c13d0c
Fatoucisse5 <fatoubcisse13@gmail.com>
2026-01-19 23:58:37 +0100

    ajout du resume de python

--- exo_python.py
+++ exo_python.py
@@ -1,7 +1,14 @@
 # on demande le nom et l'age
-nom = input("quel est votre nom")
-age = input("quel est votre age")
-print("Vous vous appelez"  + nom + ",vous avez"+ age +"ans")
-
-
-
+nom = input("quel est votre nom ? ")
+age = input("quel est votre age ? ")
+#nom = "fatou"
+#age = "20"
+print("Vous vous appelez "  + nom + ",vous avez  "+ age +"ans")
+age_prochain = int(age) + 1
+try:
+    age_prochain = int(age) + 1
+except: 
+    print("ERREUR: Vous devez rentrer un nombre pour l'age")
+else: 
+    print("Vous vous appelez" + nom +",vous avez" + str(age) +"ans")
+    print("l'an prochain vous aurez" + str(age_prochain) +"ans")      
