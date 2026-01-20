202b9ff25a7b96c7dbb74055e21406d8e7430b9b
Fatoucisse5 <fatoubcisse13@gmail.com>
2026-01-20 14:57:15 +0100

    ajout exo python

--- exo_python.py
+++ exo_python.py
@@ -1,3 +1,4 @@
+"""
 # on demande le nom et l'age
 nom = input("quel est votre nom ? ")
 age = input("quel est votre age ? ")
@@ -12,3 +13,75 @@ except:
 else: 
     print("Vous vous appelez" + nom +",vous avez" + str(age) +"ans")
     print("l'an prochain vous aurez" + str(age_prochain) +"ans")      
+
+#la boucle while
+n = 0 # cree la variable 
+while n < 10:
+    print ("Valeur de n: " + str(n))
+    n = n + 1
+    
+print("fin de la boucle")
+mot_de_passe = ""
+while not mot_de_passe == "TOTO":
+    mot_de_passe = input("quel est le mot de passe ? ")
+print("mot de passe correct, vous avez acces au compte")
+
+
+nom = input("quel est votre nom ? ")
+
+age = 0
+while age == 0:
+    age_str =input("quel est votre age ? ")
+    try: 
+        age = int(age_str)
+    except:
+        print("ERREUR : Vous devez rentrer un nombre pour l'age")
+print("Vous vous appelez" + nom +",vous avez" + str(age) + "ans")
+print("l'an prochain vous aurez" + str(age +1) + "ans")
+
+#exercice
+#demander le nom
+nom = ""
+while nom == "":
+    nom = input("entrer votre nom")
+    
+
+def demander_age ():
+    age_int = 0
+    while age_int == 0:
+        age_str = input("quel est votre age ? ")
+        try: 
+            age_int = int(age_str)
+        except:
+            print("ERREUR : Vous devez rentrer un nombre pour l'age")
+    return age_int
+
+#demander le nom
+nom = ""
+while nom == "":
+    nom = input("entrer votre nom")
+
+#demander l'age
+age = demander_age ()
+
+#afficher les resultats
+print("Vous vous appelez" + nom +",vous avez" + str(age) + "ans")
+print("l'an prochain vous aurez" + str(age +1) + "ans")
+"""
+
+"""
+#exercice
+def demander_nom() :
+    reponse_nom = ""
+    while reponse_nom == "":
+        reponse_nom = input("quel est votre nom ? ")
+    return reponse_nom
+"""    
+#exercice
+age =input("entrer un age")
+if age == 17:
+ print ("vous etes presque majeur")
+elif age == 18:
+ print("tout juste majeur;FELICITATIONS")
+else:
+ print("vous etes majeur")
